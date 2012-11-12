7a6c8f467a6fe14fba6b848a34d77d63a8cf4940
John Petroff <john.petroff@chas.se>
2012-11-12 23:55:27 +0100

    Created initial version

---
+++ SerializationStorage/SerializationStorage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SerializationStorage
+{
+    /// <summary>
+    /// SerializationStorage is a micro db that persists a xml serialized
+    /// copy of it self. When disposed it persists it self again.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SerializationStorage<T> : List<T>, IDisposable
+    {
+        readonly XmlSerializer _serializer = new XmlSerializer(typeof(List<T>));
+        readonly string _file = typeof(T).Name + ".ses";
+
+        public SerializationStorage()
+        {
+            if (!File.Exists(_file)) 
+                return;
+            
+            using (var sr = new XmlTextReader(_file))
+            {
+                var list = _serializer.Deserialize(sr) as List<T>;
+                if (list != null) 
+                    AddRange(list);
+            }
+        }
+
+        public void Dispose()
+        {
+            File.Delete(_file);
+            if (this.Any())
+            {
+                using (var stream = new FileStream(_file, FileMode.OpenOrCreate))
+                using (var fs = new StreamWriter(stream))
+                    _serializer.Serialize(fs, this);
+            }
+        }
+    }
+}
---
+++ SerializationStorageTests/StorageTests.cs
@@ -0,0 +1,120 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using SerializationStorage;
+
+namespace SerializationStorageTests
+{
+    [TestFixture]
+    public class StorageTests
+    {
+        [Test]
+        public void CreateStorageTest()
+        {
+            var test = new TestClasse
+                           {
+                               PropertyOne = "test",
+                               PropertyTwo = 1,
+                           };
+            var store = new SerializationStorage<TestClasse> {test};
+
+            Assert.IsTrue(store.Any());
+        }
+
+        /// <summary>
+        /// The purpose of this test is to see if file IO locks
+        /// </summary>
+        [Test]
+        public void TestWriteTwice()
+        {
+            var test = new TestClasse
+            {
+                PropertyOne = "test",
+                PropertyTwo = 1,
+            };
+
+            using (var store = new SerializationStorage<TestClasse>())
+            {
+                store.Add(test);
+            }
+
+            using (var store = new SerializationStorage<TestClasse>())
+            {
+                Assert.IsTrue(store.Last().PropertyOne == "test");
+                store.Last().PropertyTwo = 2;
+            }
+
+            using (var store = new SerializationStorage<TestClasse>())
+            {
+                Assert.IsTrue(store.Last().PropertyTwo == 2);
+            }
+        }
+
+        /// <summary>
+        /// Storage should selfdelete if disposed empty
+        /// </summary>
+        [Test]
+        public void CleanStorageTest()
+        {
+            var test = new TestClasse
+            {
+                PropertyOne = "test",
+                PropertyTwo = 1,
+            };
+
+            using (var store = new SerializationStorage<TestClasse>())
+            {
+                store.Add(test);
+            }
+            using (var store = new SerializationStorage<TestClasse>())
+            {
+                store.Clear();
+            }
+
+            using (var store = new SerializationStorage<TestClasse>())
+            {
+                Assert.IsFalse(store.Any());
+            }
+        }
+
+        [Test]
+        public void ShouldOverWriteFile()
+        {
+            var test = new TestClasse
+            {
+                PropertyOne = "test",
+                PropertyTwo = 1,
+            };
+
+            using (var store = new SerializationStorage<TestClasse>())
+            {
+                store.Add(test);
+                store.Add(test);
+                store.Add(test);
+                store.Add(test);
+            }
+
+            var test2 = new TestClasse
+            {
+                PropertyOne = "test2",
+                PropertyTwo = 2,
+            };
+
+            using (var store = new SerializationStorage<TestClasse>())
+            {
+                store.Clear();
+                store.Add(test2);
+            }
+
+            using (var store = new SerializationStorage<TestClasse>())
+            {
+                Assert.IsTrue(store.Last().PropertyTwo == 2);
+            }
+        }
+    }
+
+    public class TestClasse
+    {
+        public string PropertyOne { get; set; }
+        public int PropertyTwo { get; set; }
+    }
+}
